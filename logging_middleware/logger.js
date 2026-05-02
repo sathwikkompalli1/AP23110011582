d41cbae035a3a7ed36ee5c8e5035bf24a0f7929a
sathwikkompalli1 <venkatasathwik_kompalli@srmap.edu.in>
2026-05-02 11:53:48 +0530

    checking and editing

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -2,16 +2,26 @@ const axios = require("axios");
 
 const LOG_API = "http://20.207.122.201/evaluation-service/logs";
 
+const TOKEN = "eyJhbGcioiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNTcOMZQOLCJpYxQi0jE3NDM1NzQwNDQsImlzcyI6IkFmZm9yZG11ZCIsImp0aSI6ImQ5Y2JiNjk5LTZhMjctNDRhNS04ZDU5LThiMWJ1ZmE4MTZkYSIsInN1YiI6InJhbWtyaXNobmFAYWJjLmVkdSJ9LCJ1bWFpbCI6InJhbwtyaXNobmFAYWJjLmVkdSIsIm5hbwUi0iJyYW0ga3Jpc2huYSIsInJvbGxObyI6ImFhMWJiIiwiYWNjZXNzQ29kZSI6InhnQXNOQyIsImNsaWVudElEIjoiZD1jYmI20TktNmEyNy00NGE1LThkNTktOGIXYmVmYTgxNmRhIiwiY2xpZW50U2VjcmV0IjoidFZKYWFhUkJTZVhjU1h1TSJ9.YApD98gq0IN_OWw7JMfmuUfK1m4hLTm7AIcLDcLAzvg";
+
 async function Log(stack, level, pkg, message) {
     try {
-        await axios.post(LOG_API, {
-            stack,
-            level,
-            package: pkg,
-            message
-        });
-    } catch (err) {
-        console.error("Log failed:", err.message);
+        await axios.post(
+            LOG_API,
+            {
+                stack,
+                level,
+                package: pkg,
+                message
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            }
+        );
+    } catch (error) {
+        console.log("Log failed:", error.message);
     }
 }
 
--- notification_app_be/services/notificationService.js
+++ notification_app_be/services/notificationService.js
@@ -3,14 +3,21 @@ const Log = require("../../logging_middleware/logger");
 
 const API_URL = "http://20.207.122.201/evaluation-service/notifications";
 
+const TOKEN = "eyJhbGcioiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNTcOMZQOLCJpYxQi0jE3NDM1NzQwNDQsImlzcyI6IkFmZm9yZG11ZCIsImp0aSI6ImQ5Y2JiNjk5LTZhMjctNDRhNS04ZDU5LThiMWJ1ZmE4MTZkYSIsInN1YiI6InJhbWtyaXNobmFAYWJjLmVkdSJ9LCJ1bWFpbCI6InJhbwtyaXNobmFAYWJjLmVkdSIsIm5hbwUi0iJyYW0ga3Jpc2huYSIsInJvbGxObyI6ImFhMWJiIiwiYWNjZXNzQ29kZSI6InhnQXNOQyIsImNsaWVudElEIjoiZD1jYmI20TktNmEyNy00NGE1LThkNTktOGIXYmVmYTgxNmRhIiwiY2xpZW50U2VjcmV0IjoidFZKYWFhUkJTZVhjU1h1TSJ9.YApD98gq0IN_OWw7JMfmuUfK1m4hLTm7AIcLDcLAzvg";
 async function fetchNotifications() {
     try {
-        const response = await axios.get(API_URL);
-        await Log("backend", "info", "service", "Fetched notifications successfully");
+        const response = await axios.get(API_URL, {
+            headers: {
+                Authorization: `Bearer ${TOKEN}`
+            }
+        });
+
+        await Log("backend", "info", "service", "Notifications fetched");
 
         return response.data.notifications;
     } catch (error) {
         await Log("backend", "error", "service", "Failed to fetch notifications");
+        console.log("Fetch error:", error.message);
         return [];
     }
 }
